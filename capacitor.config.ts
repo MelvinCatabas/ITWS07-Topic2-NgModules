5d28c2f22be2177866b81e827492a0092ee995bd
gelcunanan <angelitoicunananjr@gmail.com>
2025-08-29 10:38:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Topic2-NgModules',
+  webDir: 'www'
+};
+
+export default config;
